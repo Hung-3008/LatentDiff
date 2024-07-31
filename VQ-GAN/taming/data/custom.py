ca609dc94e34d328b7ffc18ffbddd1a3acc95b89
hungbv16 <hungbv16@fpt.com>
2024-07-31 09:59:47 +0700

    Remove large checkpoint files from Git index

---
+++ VQ-GAN/taming/data/custom.py
@@ -0,0 +1,71 @@
+import os
+from torch.utils.data import Dataset
+
+from monai import transforms
+from monai.data import Dataset as MonaiDataset
+
+
+def transform_func(x):
+    return x[0, :, :, :]
+
+brats_transforms = transforms.Compose(
+    [
+        transforms.LoadImaged(keys=["t1", "t1ce", "t2", "flair"], allow_missing_keys=True),
+        transforms.EnsureChannelFirstd(keys=["t1", "t1ce", "t2", "flair"], allow_missing_keys=True),
+        transforms.Lambdad(keys=["t1", "t1ce", "t2", "flair"], func=transform_func),
+        transforms.EnsureChannelFirstd(keys=["t1", "t1ce", "t2", "flair"]),
+        transforms.EnsureTyped(keys=["t1", "t1ce", "t2", "flair"]),
+        transforms.Orientationd(keys=["t1", "t1ce", "t2", "flair"], axcodes="RAI", allow_missing_keys=True),
+        transforms.CropForegroundd(keys=["t1", "t1ce", "t2", "flair"], source_key="t1", allow_missing_keys=True),
+        transforms.SpatialPadd(keys=["t1", "t1ce", "t2", "flair"], spatial_size=(160, 160, 128), allow_missing_keys=True),
+        transforms.RandSpatialCropd( keys=["t1", "t1ce", "t2", "flair"],
+            roi_size=(160, 160, 128),
+            random_center=True, 
+            random_size=False,
+        ),
+        transforms.ScaleIntensityRangePercentilesd(keys=["t1", "t1ce", "t2", "flair"], lower=0, upper=99.75, b_min=0, b_max=1),
+    ]
+)
+
+def get_brats_dataset(data_path):
+    transform = brats_transforms 
+    
+    data = []
+    for subject in os.listdir(data_path):
+        sub_path = os.path.join(data_path, subject)
+        if os.path.exists(sub_path) == False: continue
+        t1 = os.path.join(sub_path, f"{subject}_t1.nii") 
+        t1ce = os.path.join(sub_path, f"{subject}_t1ce.nii") 
+        t2 = os.path.join(sub_path, f"{subject}_t2.nii") 
+        flair = os.path.join(sub_path, f"{subject}_flair.nii") 
+        seg = os.path.join(sub_path, f"{subject}_seg.nii")
+
+        data.append({"t1":t1, "t1ce":t1ce, "t2":t2, "flair":flair, "subject_id": subject})
+                    
+    print("num of subject:", len(data))
+
+    return MonaiDataset(data=data, transform=transform)
+
+
+
+
+class CustomBase(Dataset):
+    def __init__(self,data_path):
+        super().__init__()
+        self.data = get_brats_dataset(data_path)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+
+class CustomTrain(CustomBase):
+    def __init__(self, data_path, **kwargs):
+        super().__init__(data_path=data_path)
+
+
+class CustomTest(CustomBase):
+    def __init__(self, data_path, **kwargs):
+        super().__init__(data_path=data_path)
